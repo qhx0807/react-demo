9a2b5227a0a1119b65bc89f0a8d93ba50d5a2683
qhx0807 <820240134@qq.com>
2018-08-02 17:42:17 +0800

    some update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
 import { Button } from 'antd'
+import axios from 'axios'
 
 class Home extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      a: 12,
+      loading: false
+    }
+  }
   render () {
     return (
       <div>
-        <h2>Home</h2>
-        <Button>Home Button</Button>
+        <h2>{this.state.a}</h2>
+        <Button loading={this.state.loading} onClick={this.clickHandler.bind(this)}>Home Button</Button>
       </div>
     )
+  }
+  componentDidMount () {
+    console.log(1)
+  }
+  async clickHandler () {
+    this.setState({loading: true})
+    let d = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    console.log(d)
+    this.setState({loading: false})
   }
 }
 
