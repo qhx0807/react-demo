e365a7486f09136ee279baa6b93913e7c23c6b98
qhx0807 <820240134@qq.com>
2018-08-26 23:41:35 +0800

    add main Layout

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Loadable from 'react-loadable'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Loading from '../components/Loading/Loading'
 const User = Loadable({ loader: () => import('../components/User/User'), loading: Loading })
+const Main = Loadable({ loader: () => import('../components/Main/Main'), loading: Loading })
 
 class RouterConfig extends Component {
   render () {
@@ -11,6 +12,7 @@ class RouterConfig extends Component {
         <Switch>
           <Route exact path="/" component={User}></Route>
           <Route path="/user" component={User}></Route>
+          <Route path="/main" component={Main}></Route>
         </Switch>
       </Router>
     )
