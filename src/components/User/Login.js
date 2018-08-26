2ef5ce455039c99507a841aac91d418a5c22c1bc
qhx0807 <820240134@qq.com>
2018-08-26 23:25:07 +0800

    login

--- src/components/User/Login.js
+++ src/components/User/Login.js
@@ -1,28 +1,75 @@
 import React, { Component } from 'react'
-import { Button } from 'antd'
+import { Form, Icon, Input, Button, Checkbox } from 'antd'
 import styles from './Login.less'
+import logo from '../../assets/logo.svg'
+const FormItem = Form.Item
 
 class Login extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      a: 'Login'
+      loading: false
     }
   }
   render () {
+    const { getFieldDecorator } = this.props.form
+    const rules = {
+      userName: [{ required: true, message: '请输入用户名' }],
+      password: [{ required: true, message: '请输入密码' }]
+    }
     return (
       <div className={styles.login}>
-        <div className={styles.loginBox}>121</div>
-        <Button>Login</Button>
-      </div>
+        <div className={styles.loginTop}>
+          <div className={styles.topHeader}>
+            <a href="#/">
+              <img src={logo} alt="" />
+              <span>Ant Design</span>
+            </a>
+          </div>
+          <div className={styles.topDesc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div>
+        </div>
+        <Form onSubmit={this.handleSubmit} className={styles.loginForm} autocomplete="off">
+          <FormItem>
+            {
+              getFieldDecorator('userName', { rules: rules.userName })(
+                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="请输入用户名" />)
+            }
+          </FormItem>
+          <FormItem>
+            {
+              getFieldDecorator('password', { rules: rules.password })(
+                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="请输入密码" />)
+            }
+          </FormItem>
+          <FormItem>
+            {
+              getFieldDecorator('remember', { valuePropName: 'checked', initialValue: true })(
+                <Checkbox>记住账号</Checkbox>)
+            }
+            <a className={styles.loginFormForgot} href="">忘记密码</a>
+            <Button type="primary" loading={this.state.loading} htmlType="submit" className={styles.loginFormButton}>登录</Button>
+            <a href="" className={styles.loginFormReg}>注册账号</a>
+          </FormItem>
+        </Form>
+      </div>  
     )
   }
   componentDidMount () {
-    console.log(styles)
+    // console.log(styles)
+    // this.props.form.validateFields()
   }
-  clickHandler = () => {
-    this.props.history.push('/about')
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        this.setState({ loading: true })
+        setTimeout(() => {
+          this.setState({ loading: false })
+          this.props.history.push('/home')
+        }, 1000)
+      }
+    })
   }
 }
 
-export default Login
+export default Form.create()(Login)
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react'
 import Loadable from 'react-loadable'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Login from '../components/User/Login'
 import Loading from '../components/Loading/Loading'
-const Home = Loadable({ loader: () => import('../components/Home'), loading: Loading })
-const About = Loadable({ loader: () => import('../components/About'), loading: Loading })
 const User = Loadable({ loader: () => import('../components/User/User'), loading: Loading })
 
 class RouterConfig extends Component {
@@ -12,10 +9,7 @@ class RouterConfig extends Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Login}></Route>
-          <Route path="/about" component={About}></Route>
-          <Route path="/home" component={Home}></Route>
-          <Route path="/Loading" component={Loading}></Route>
+          <Route exact path="/" component={User}></Route>
           <Route path="/user" component={User}></Route>
         </Switch>
       </Router>
